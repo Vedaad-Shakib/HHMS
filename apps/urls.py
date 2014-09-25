ab9336fe46ae13f4b8b40d22565069bff0a6cf72
Vedaad Shakib <vedaad799@gmail.com>
2014-09-24 19:13:49 -0700

    Added parsing of login file for correct redirection and verification

--- apps/urls.py
+++ apps/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns
 from django.conf.urls import include
 
 urlpatterns = patterns('hhms.views',
-    url(r'^$',            'home'       ),
+    url(r'^$',             'home'       ),
     url(r'^loginSubmit/$', 'loginSubmit'),
 )
--- updateLogin.py
+++ updateLogin.py
@@ -3,9 +3,10 @@
 import urllib
 import urllib2
 import cookielib
+import re
 
 def updateLogin():
-    # for some REALLY odd reason, urllib.urlopen(url) returns a fake site (probably PCR precautions)
+    # for some REALLY odd reason, urllib.urlopen(url) returns error for PCR (and not for other sites)
     # you have to send a request instead
     cj = cookielib.CookieJar()
     opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
@@ -13,7 +14,7 @@ def updateLogin():
     urllib2.install_opener(opener)
     url = 'https://webappsca.pcrsoft.com/Clue/Student-Portal-Login-LDAP/8464?returnUrl=https%3a%2f%2fwebappsca.pcrsoft.com%2fClue%2fDefault.aspx%3fpid%3d7536'
     
-    # fake so we can just get form
+    # random form just to send request
     auth = {
         'user': '<username>',
         'password': '<password>'
@@ -21,7 +22,15 @@ def updateLogin():
     data = urllib.urlencode(auth)
     req = urllib2.Request(url, data)
     loginSite = urllib2.urlopen(req)
-    loginContents = loginSite.read().replace("8464", "loginSubmit/")
+    loginContents = loginSite.read()
+    loginContents = re.sub("action[ ]*=[ ]*['\"].*?['\"]", "action=\"loginSubmit/\"", loginContents, count=1)
+
+    # add csrf token
+    formTag = re.findall("<form.*?>", loginContents)[0]
+    tagIndex = loginContents.index(formTag)
+    insertIndex = tagIndex + len(formTag) + 1
+    loginContents = loginContents[:insertIndex+1] + "{% csrf_token %}" + loginContents[insertIndex+1:]
+    
     
     loginFile = open("templates/login.html", "w")
     loginFile.seek(0)
